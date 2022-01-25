3dc917504f0aca5d7b40f53154fd65d549e53ec0
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2022-01-25 14:40:46 +0300

    init project

---
+++ ColorSliders/ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersApp.swift
+//  ColorSliders
+//
+//  Created by Михаил Зиновьев on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
